4391a5672eb161a52d66a81cbc0782fd7a1357f9
Olexandr Lytvynenko <litvinenko1978aleks@gmail.com>
2026-04-11 15:05:04 +0300

    add new function to card Valveditails and filtered Array by string to null

--- src/components/ValveDetails/ValveDetails.jsx
+++ src/components/ValveDetails/ValveDetails.jsx
@@ -15,6 +15,51 @@ const ValveDetails = () => {
   const valve = items.find(item => String(item.id) === id);
 
   if (!valve) return <p>No characteristics found valve </p>;
+  //new constant isValidValue
+  const isValidValue = value => {
+    return value !== null && value !== undefined && value !== "";
+  };
+
+  // objects= Array
+  const fields = [
+    // весь блок - масив 26 значень!
+    { key: "Size", label: t("valvedetails.Size") },
+    { key: "Function_Principle", label: t("valvedetails.FunctionPrinciple") },
+    { key: "Advantages", label: t("valvedetails.Advantages") },
+    { key: "Nominal_pressure", label: t("valvedetails.NominalPr") },
+    { key: "Operating_pressure", label: t("valvedetails.OperatingPr") },
+    { key: "Minimum_open_pressure", label: t("valvedetails.MinimumOpPr") },
+    { key: "Control_pressure", label: t("valvedetails.Control_pressure") },
+    { key: "Applicable_medium", label: t("valvedetails.Applicable_medium") },
+    { key: "Applicable_fluid", label: t("valvedetails.Applicable_fluid") },
+    { key: "Control_medium", label: t("valvedetails.Control_medium") },
+    { key: "Control_fluid", label: t("valvedetails.Control_fluid") },
+    { key: "Medium_temperature", label: t("valvedetails.Medium_temperature") },
+    { key: "Fluid_temperature", label: t("valvedetails.Fluid_temperature") },
+    {
+      key: "Ambient_temperature",
+      label: t("valvedetails.Ambient_temperature"),
+    },
+    { key: "Seal_material", label: t("valvedetails.Seal_material") },
+    {
+      key: "Seal_ring_and_packing_material",
+      label: t("valvedetails.Seal_R_P_material"),
+    },
+    { key: "Disc_material", label: t("valvedetails.Disc_material") },
+    { key: "Actuator_material", label: t("valvedetails.Actuator_material") },
+    { key: "Ball_material", label: t("valvedetails.Ball_material") },
+    { key: "Body_material", label: t("valvedetails.Body_material") },
+    { key: "Cylinder_material", label: t("valvedetails.Cylinder_material") },
+    { key: "Control_type", label: t("valvedetails.Control_type") },
+    { key: "Connection_type", label: t("valvedetails.Connection_type") },
+    { key: "Flange_standards", label: t("valvedetails.Flange_standards") },
+    { key: "Structure_lenght", label: t("valvedetails.Structure_lenght") },
+    { key: "Leakage_class", label: t("valvedetails.Leakage_class") },
+  ];
+  // filters for map
+  const filteredFields = fields.filter(field => isValidValue(valve[field.key]));
+
+  const allArrayFields = filteredFields.slice(0, 27);
 
   return (
     <container className={css.containerList}>
@@ -26,122 +71,14 @@ const ValveDetails = () => {
 
         <section className={css.cartContainer}>
           <ul className={css.cartComent}>
-            <li className={css.descrip}>
-              <strong>{t("valvedetails.Size")}:&nbsp;</strong>
-              {valve.Size}
-            </li>
-            <li className={css.descrip}>
-              <strong>{t("valvedetails.FunctionPrinciple")}:&nbsp;</strong>
-              {valve.Function_Principle}
-            </li>
-            <li className={css.descrip}>
-              <strong>{t("valvedetails.Advantages")}:&nbsp;</strong>
-              {valve.Advantages}
-            </li>
-            <li className={css.descrip}>
-              <strong>{t("valvedetails.NominalPr")}:&nbsp;</strong>
-              {valve.Nominal_pressure}
-            </li>
-            <li className={css.descrip}>
-              <strong>{t("valvedetails.OperatingPr")}:&nbsp;</strong>
-              {valve.Operating_pressure}
-            </li>
-            <li className={css.descrip}>
-              <strong>{t("valvedetails.MinimumOpPr")}:&nbsp;</strong>
-              {valve.Minimum_open_pressure}
-            </li>
-            <li className={css.descrip}>
-              <strong>{t("valvedetails.Control_pressure")}:&nbsp;</strong>
-              {valve.Control_pressure}
-            </li>
-            <li className={css.descrip}>
-              <strong>{t("valvedetails.Applicable_medium")}:&nbsp;</strong>
-              {valve.Applicable_medium}
-            </li>
-            <li className={css.descrip}>
-              <strong>{t("valvedetails.Applicable_fluid")}:&nbsp;</strong>
-              {valve.Applicable_fluid}
-            </li>
-            <li className={css.descrip}>
-              <strong>{t("valvedetails.Control_medium")}:&nbsp;</strong>
-              {valve.Control_medium}
-            </li>
-            {/*залишаю рядків с права від зображення і переношу */}
+            {allArrayFields.map(field => (
+              <li key={field.key} className={css.descrip}>
+                <strong>{field.label}:&nbsp;</strong>
+                {valve[field.key]}
+              </li>
+            ))}
           </ul>
         </section>
-      </section>
-
-      <section className={css.cartContainerBottom}>
-        <ul className={css.cartComentBottom}>
-          {/*залишаю по 8 в блоку і переношу */}
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Control_fluid")}:&nbsp;</strong>
-            {valve.Control_fluid}
-          </li>
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Medium_temperature")}:&nbsp;</strong>
-            {valve.Medium_temperature}
-          </li>
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Fluid_temperature")}:&nbsp;</strong>
-            {valve.Fluid_temperature}
-          </li>
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Ambient_temperature")}:&nbsp;</strong>
-            {valve.Ambient_temperature}
-          </li>
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Seal_material")}:&nbsp;</strong>
-            {valve.Seal_material}
-          </li>
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Seal_R_P_material")}:&nbsp;</strong>
-            {valve.Seal_ring_and_packing_material}
-          </li>
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Disc_material")}:&nbsp;</strong>
-            {valve.Disc_material}
-          </li>
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Actuator_material")}:&nbsp; </strong>
-            {valve.Actuator_material}
-          </li>
-        </ul>
-        {/* переношу на другий ряд */}
-        <ul className={css.cartComentBottom}>
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Ball_material")}:&nbsp; </strong>
-            {valve.Ball_material}
-          </li>
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Body_material")}:&nbsp; </strong>
-            {valve.Body_material}
-          </li>
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Cylinder_material")}:&nbsp; </strong>
-            {valve.Cylinder_material}
-          </li>
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Control_type")}:&nbsp; </strong>
-            {valve.Control_type}
-          </li>
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Connection_type")}:&nbsp; </strong>
-            {valve.Connection_type}
-          </li>
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Flange_standards")}:&nbsp; </strong>
-            {valve.Flange_standards}
-          </li>
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Structure_lenght")}:&nbsp; </strong>
-            {valve.Structure_lenght}
-          </li>
-          <li className={css.descrip}>
-            <strong>{t("valvedetails.Leakage_class")}:&nbsp; </strong>
-            {valve.Leakage_class}
-          </li>
-        </ul>
       </section>
 
       <button className={css.buttonIcon}>
@@ -172,3 +109,120 @@ const ValveDetails = () => {
 };
 
 export default ValveDetails;
+
+// <ul className={css.cartComent}>
+//   <li className={css.descrip}>
+//     <strong>{t("valvedetails.Size")}:&nbsp;</strong>
+//     {valve.Size}
+//   </li>
+//   <li className={css.descrip}>
+//     <strong>{t("valvedetails.FunctionPrinciple")}:&nbsp;</strong>
+//     {valve.Function_Principle}
+//   </li>
+//   <li className={css.descrip}>
+//     <strong>{t("valvedetails.Advantages")}:&nbsp;</strong>
+//     {valve.Advantages}
+//   </li>
+//   <li className={css.descrip}>
+//     <strong>{t("valvedetails.NominalPr")}:&nbsp;</strong>
+//     {valve.Nominal_pressure}
+//   </li>
+//   <li className={css.descrip}>
+//     <strong>{t("valvedetails.OperatingPr")}:&nbsp;</strong>
+//     {valve.Operating_pressure}
+//   </li>
+//   <li className={css.descrip}>
+//     <strong>{t("valvedetails.MinimumOpPr")}:&nbsp;</strong>
+//     {valve.Minimum_open_pressure}
+//   </li>
+//   <li className={css.descrip}>
+//     <strong>{t("valvedetails.Control_pressure")}:&nbsp;</strong>
+//     {valve.Control_pressure}
+//   </li>
+//   <li className={css.descrip}>
+//     <strong>{t("valvedetails.Applicable_medium")}:&nbsp;</strong>
+//     {valve.Applicable_medium}
+//   </li>
+//   <li className={css.descrip}>
+//     <strong>{t("valvedetails.Applicable_fluid")}:&nbsp;</strong>
+//     {valve.Applicable_fluid}
+//   </li>
+//   <li className={css.descrip}>
+//     <strong>{t("valvedetails.Control_medium")}:&nbsp;</strong>
+//     {valve.Control_medium}
+//   </li>
+//   {/*залишаю рядків с права від зображення і переношу */}
+// </ul>;
+
+// <section className={css.cartContainerBottom}>
+//   <ul className={css.cartComentBottom}>
+//     {/*залишаю по 8 в блоку і переношу */}
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Control_fluid")}:&nbsp;</strong>
+//       {valve.Control_fluid}
+//     </li>
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Medium_temperature")}:&nbsp;</strong>
+//       {valve.Medium_temperature}
+//     </li>
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Fluid_temperature")}:&nbsp;</strong>
+//       {valve.Fluid_temperature}
+//     </li>
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Ambient_temperature")}:&nbsp;</strong>
+//       {valve.Ambient_temperature}
+//     </li>
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Seal_material")}:&nbsp;</strong>
+//       {valve.Seal_material}
+//     </li>
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Seal_R_P_material")}:&nbsp;</strong>
+//       {valve.Seal_ring_and_packing_material}
+//     </li>
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Disc_material")}:&nbsp;</strong>
+//       {valve.Disc_material}
+//     </li>
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Actuator_material")}:&nbsp; </strong>
+//       {valve.Actuator_material}
+//     </li>
+//   </ul>
+//   {/* переношу на другий ряд */}
+//   <ul className={css.cartComentBottom}>
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Ball_material")}:&nbsp; </strong>
+//       {valve.Ball_material}
+//     </li>
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Body_material")}:&nbsp; </strong>
+//       {valve.Body_material}
+//     </li>
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Cylinder_material")}:&nbsp; </strong>
+//       {valve.Cylinder_material}
+//     </li>
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Control_type")}:&nbsp; </strong>
+//       {valve.Control_type}
+//     </li>
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Connection_type")}:&nbsp; </strong>
+//       {valve.Connection_type}
+//     </li>
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Flange_standards")}:&nbsp; </strong>
+//       {valve.Flange_standards}
+//     </li>
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Structure_lenght")}:&nbsp; </strong>
+//       {valve.Structure_lenght}
+//     </li>
+//     <li className={css.descrip}>
+//       <strong>{t("valvedetails.Leakage_class")}:&nbsp; </strong>
+//       {valve.Leakage_class}
+//     </li>
+//   </ul>
+// </section>;
